77e9b07227dba4d36051eaae184fcbeb3736631e
Gabriela <gabrielamzuppardo@gmail.com>
2023-11-13 20:41:06 -0300

    adicionando imagens na pagina de home

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Image, View } from 'react-native';
 import { Appbar, Button, Card, Title, Paragraph, Provider as PaperProvider } from 'react-native-paper';
-
+import sombrinha from '../../assets/photo-sombrinha.png'
+import pacoFrevo from '../../assets/photo-paco.png'
 const HomeScreen = () => {
   return (
     <PaperProvider>
       <ScrollView style={styles.container}>
-      <Title style={styles.title}>CONECTE-SE COM A GENTE!</Title>
-          <View style={styles.viewText}>
-            <Title style={styles.textTitle}>SOBRE</Title>
-            <Paragraph style={styles.textParagraph}>Esse website pertence ao espaço cultural Paço do Frevo no qual foi criado com o objetivo de localizar e mapear agremiações nacionalmente e blabla
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Paragraph>
+        <Title style={styles.title}>CONECTE-SE COM A GENTE!</Title>
+        <View style={styles.viewText}>
+          <Title style={styles.textTitle}>SOBRE</Title>
+          <Paragraph style={styles.textParagraph}>Esse website pertence ao espaço cultural Paço do Frevo no qual foi criado com o objetivo de localizar e mapear agremiações nacionalmente e blabla
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Paragraph>
+          <View style={{ flexDirection: 'row', marginTop: 30, display: 'flex', justifyContent:'center' }}>
+            <Image 
+              source={sombrinha}
+              style={{position: 'relative', left: 10, width: 100, height: 100, marginRight: 10, zIndex: 1 }}
+            />
+            <Image
+              source={pacoFrevo}
+              style={{ position: 'relative', right: 25, width: 100, height: 100 }}
+            />
           </View>
+        </View>
       </ScrollView>
     </PaperProvider>
   );
@@ -22,7 +33,8 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   viewText: {
-    height: 300,
+    height: 350,
+  
   },
   textTitle: {
     color: 'red',
