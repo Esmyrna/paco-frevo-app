1e15701cfbcad74ca30dd96461b67aedd8c73014
Gabriela <esmyrnaoliveira13@gmail.com>
2023-11-13 20:58:41 -0300

    criando botões na home

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Image, View } from 'react-native';
+import { ScrollView, StyleSheet, Image, View, TouchableOpacity, Text } from 'react-native';
 import { Appbar, Button, Card, Title, Paragraph, Provider as PaperProvider } from 'react-native-paper';
 import sombrinha from '../../assets/photo-sombrinha.png'
 import pacoFrevo from '../../assets/photo-paco.png'
@@ -12,16 +12,23 @@ const HomeScreen = () => {
           <Title style={styles.textTitle}>SOBRE</Title>
           <Paragraph style={styles.textParagraph}>Esse website pertence ao espaço cultural Paço do Frevo no qual foi criado com o objetivo de localizar e mapear agremiações nacionalmente e blabla
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Paragraph>
-          <View style={{ flexDirection: 'row', marginTop: 30, display: 'flex', justifyContent:'center' }}>
-            <Image 
+          <View style={{ flexDirection: 'row', marginTop: 30, display: 'flex', justifyContent: 'center', marginBottom: 30 }}>
+            <Image
               source={sombrinha}
-              style={{position: 'relative', left: 10, width: 100, height: 100, marginRight: 10, zIndex: 1 }}
+              style={{ position: 'relative', left: 10, width: 100, height: 100, marginRight: 10, zIndex: 1 }}
             />
             <Image
               source={pacoFrevo}
               style={{ position: 'relative', right: 25, width: 100, height: 100 }}
             />
           </View>
+          <Title style={styles.title}>O QUE DESEJAS?</Title>
+          <TouchableOpacity style={{ marginTop: 10, backgroundColor: '#27962D', padding: 10, borderRadius: 5 }}>
+            <Text style={{ textAlign: 'center', color: '#fff' }}>CADASTRAR AGREMIAÇÃO</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ marginTop: 10, backgroundColor: '#00377B', padding: 10, borderRadius: 5 }}>
+            <Text style={{ textAlign: 'center', color: '#fff' }}>DASHBOARD</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </PaperProvider>
@@ -33,8 +40,8 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   viewText: {
-    height: 350,
-  
+    height: 550,
+
   },
   textTitle: {
     color: 'red',
@@ -43,7 +50,7 @@ const styles = StyleSheet.create({
   title: {
     textAlign: 'center',
     fontWeight: 600,
-    color: 'red'
+    color: 'red',
   },
   textParagraph: {
     color: '#000',
