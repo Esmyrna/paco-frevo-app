9697046443b894126ed0f27accc35b28d5190335
Esmyrna <esmyrnaoliveira13@gmail.com>
2023-11-23 20:10:38 -0300

    criando conexão com o back usando react query

---
+++ api/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pacodofrevoapi1-6ka9yo5l.b4a.run';
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const fetchAssociations = async (page = 1, pageSize = 10) => {
+  try {
+    const response = await api.get(`/associations/paged?page=${page}&pageSize=${pageSize}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching associations:', error);
+    throw error;  
+  }
+};
