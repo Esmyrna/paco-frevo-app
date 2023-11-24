b84417e6107b6dbc712be9febfb8543672f4ee58
1nathalia23 <bacalhaunathalia@gmail.com>
2023-11-23 21:05:04 -0300

    estilizando a lista renderizada

--- src/pages/ListAllAssociations.js
+++ src/pages/ListAllAssociations.js
@@ -2,43 +2,67 @@ import React, { useState } from 'react';
 import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { useQuery } from 'react-query';
 import { fetchAssociations } from '../../api/api'
+import { Card } from 'react-native-paper';
 
 const ListAllAssociations = () => {
-  const [page, setPage] = useState(1);
+    const [page, setPage] = useState(1);
 
-  const { data: associations, isFetchingMore } = useQuery(
-    ['associations', page],
-    () => fetchAssociations(page),
-    {
-      staleTime: 60000,
-      getNextPageParam: (lastPage, pages) => {
-        return lastPage.hasNextPage ? lastPage.pageIndex + 1 : null;
-      },
-    }
-  );
+  
 
-  return (
-    <View style={styles.container}>
-      <Text>Associações Cadastradas:</Text>
-      <FlatList
-        data={associations?.result || []}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{`ID: ${item.id}`}</Text>
-            <Text>{`Nome: ${item.name}`}</Text>
-          </View>
-        )}
-        ListFooterComponent={isFetchingMore && <Text>Carregando mais...</Text>}
-      />
-    </View>
-  );
+    const { data: associations, isFetchingMore } = useQuery(
+        ['associations', page],
+        () => fetchAssociations(page),
+        {
+            staleTime: 60000,
+            getNextPageParam: (lastPage, pages) => {
+                return lastPage.hasNextPage ? lastPage.pageIndex + 1 : null;
+            },
+        }
+    );
+
+    return (
+        <View style={styles.container}>
+            <Text>Associações Cadastradas:</Text>
+            <FlatList
+                data={associations?.result || []}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <View>
+                        <Card style={styles.card}>
+                            <Text style={styles.cardText}> 
+                             <Text>Nome: </Text>
+                                {item.name}
+                            </Text>
+                            <Text>
+                            <Text>Data de Fundação: </Text>
+                                {item.foundationDate}
+                            </Text>
+                            <Text>
+                            <Text>Cores: </Text>
+                                {item.colors}
+                            </Text>
+                            <Text>
+                            <Text>Tipo de Associação: </Text>
+                               {item.associationType}
+                            </Text>
+                        </Card>
+                    </View>
+                )}
+                ListFooterComponent={isFetchingMore && <Text>Carregando mais...</Text>}
+            />
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-  },
+    container: {
+        padding: 16,
+    },
+    card: {
+        backgroundColor: '#fff',
+        padding: 20,
+        marginTop: 10,
+    }
 });
 
 export default ListAllAssociations;
