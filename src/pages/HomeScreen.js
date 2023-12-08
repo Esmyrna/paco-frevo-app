48d0b8397eb029c4cfd2d6ec9dc180c9e1f2affc
DALuckwu <dara.00000847256@unicap.br>
2023-12-08 16:44:05 -0300

    Ajustes nas páginas Login e Home

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Image, View, TouchableOpacity, Text } from 'react-native';
+import { ScrollView, StyleSheet, Image, View, TouchableOpacity, Text, Linking } from 'react-native';
 import { Title, Paragraph, Provider as PaperProvider } from 'react-native-paper';
 import sombrinha from '../../assets/photo-sombrinha.png'
 import pacoFrevo from '../../assets/photo-paco.png'
 import detalhes from '../../assets/detalhes.png'
+import simbolo from '../../assets/simbols.png'
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -27,31 +29,84 @@ const HomeScreen = () => {
 
 
   return (
-    <PaperProvider style={styles.container}>
+    <PaperProvider>
       <ScrollView style={styles.container}>
+        {/* Título da página */}
         <Title style={styles.title}>CONECTE-SE COM A GENTE!</Title>
-        <View style={styles.viewText}>
-    
-          <Paragraph style={styles.textParagraph}>Esse website pertence ao espaço cultural Paço do Frevo no qual foi criado com o objetivo de localizar e mapear agremiações nacionalmente e blabla
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</Paragraph>
-          <View style={{ flexDirection: 'row', marginTop: 30, display: 'flex', justifyContent: 'center', marginBottom: 30 }}>
-           
+        <ScrollView style={styles.viewText}>
+          {/* Subtítulo 'SOBRE' */}
+          <View style={styles.textTitle}>
+            <Title style={styles.title}>SOBRE O PAÇO DO FREVO</Title>
+            <Image source={simbolo} style={styles.simbolo} />
           </View>
-          <Title style={styles.title}>O QUE DESEJAS?</Title>
-          <TouchableOpacity onPress={handleForm} style={{ marginTop: 10, backgroundColor: '#00377B', padding: 10, borderRadius: 5 }}>
+          {/* Informações sobre o paço do frevo */}
+          <View style={styles.informations}>
+            <View style={styles.contentAlignWithImage}>
+              <Paragraph style={styles.textParagraph}>
+                O Paço do Frevo é reconhecido pelo Iphan como centro de referência em ações, projetos, transmissão, salvaguarda 
+                e valorização de uma das principais tradições culturais do Brasil, o Frevo. 
+              </Paragraph>
+              <Image source={pacoFrevo} style={styles.pacoFrevo}/>
+            </View>
+            <View style={styles.contentAlignWithImage}>
+              <Image source={sombrinha} />
+              <Paragraph style={styles.textParagraphToRight}>
+                Patrimônio imaterial pela Unesco e pelo Iphan, o Frevo é um convite à celebração da vida, por meio da ativação de memórias, personalidades e linguagens artísticas, 
+                que no Paço do Frevo encontram seu lugar máximo de expressão, na manutenção de ações de difusão, pesquisa e formação nas áreas da dança e da música, dos adereços e 
+                das agremiações do Frevo.
+              </Paragraph>
+            </View>
+
+            <View style={styles.textTitle}>
+              <Title style={styles.title}>O QUE É O PROJETO?</Title>
+              <Image source={simbolo} style={styles.simbolo} />
+            </View>
+
+            <Paragraph style={styles.textParagraph}>
+              O Paço do Frevo está montando um banco de dados para mapear 
+              quem faz a manifestação popular Patrimônio Imaterial do Brasil e 
+              da humanidade, dentro e fora do Carnaval! 
+            </Paragraph>
+            <Paragraph style={styles.textParagraph}>
+              Cadastre sua agremiação, troça, clube, bloco, 
+              orquestra, grupo, atividade artística ou produção cultural e 
+              siga conosco no bloco de quem mantém o Frevo vivo o ano todo!
+            </Paragraph>
+          </View>
+          
+          <TouchableOpacity onPress={handleForm} style={{ margin: 16, backgroundColor: '#27962D', padding: 10, borderRadius: 8, width: 'auto' }}>
             <Text style={{ textAlign: 'center', color: '#fff' }}>CADASTRAR AGREMIAÇÃO</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{ marginTop: 10, backgroundColor: '#DCDCDC', padding: 10, borderRadius: 5 }}>
-            <Text onPress={handleListAll} style={{ textAlign: 'center', color: '#000' }}>AGREMIAÇÕES CADASTRADAS</Text>
+          <TouchableOpacity style={{ margin: 16, backgroundColor: '#00377B', padding: 10, borderRadius: 8, width: 'auto' }}>
+            <Text onPress={handleCreateEntity} style={{ textAlign: 'center', color: '#FFF' }}>CADASTRAR FREVO MAKERS</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{ marginTop: 10, backgroundColor: '#DCDCDC', padding: 10, borderRadius: 5 }}>
-            <Text onPress={handleCreateEntity} style={{ textAlign: 'center', color: '#000' }}>CADASTRAR FREVO MAKERS</Text>
+          {/* <View style={{ flexDirection: 'row', marginTop: 30, display: 'flex', justifyContent: 'center', marginBottom: 30 }}>
+           
+          </View> */}
+          <Title style={styles.title}>NÃO É ISSO QUE DESEJAS?</Title>
+          <TouchableOpacity style={{ margin: 16, backgroundColor: '#27962D', padding: 10, borderRadius: 8, width: 'auto' }}>
+            <Text onPress={handleListAll} style={{ textAlign: 'center', color: '#fff' }}>AGREMIAÇÕES CADASTRADAS</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{ marginTop: 10, backgroundColor: '#00377B', padding: 10, borderRadius: 5 }}>
+          <TouchableOpacity style={{ margin: 16, backgroundColor: '#00377B', padding: 10, borderRadius: 8, width: 'auto' }}>
             <Text onPress={handleListAllFrevo} style={{ textAlign: 'center', color: '#fff' }}>FREVO MAKERS CADASTRADAS</Text>
           </TouchableOpacity>
-  
-        </View>
+
+          {/* Rodapé com ícones */}
+          <View style={styles.footer}>
+            <TouchableOpacity onPress={() => Linking.openURL('https://www.youtube.com/@PacodoFrevoMuseu')}>
+              <Icon name="youtube" size={30} color="#FF0000" />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Linking.openURL('https://pacodofrevo.org.br')}>
+              <Icon name="google" size={30} color="#4285F4" />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={Linking.openURL('facebook.com/pacodofrevo')}>
+              <Icon name="facebook" size={30} color="#1877f2" />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => Linking.openURL('https://instagram.com/pacodofrevo')}>
+              <Icon name="instagram" size={30} color="#833AB4" />
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </ScrollView>
     </PaperProvider>
   );
@@ -60,26 +115,93 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     padding: 16,
-    backgroundColor: 'linear-gradient(0deg, rgba(166,15,15,1) 0%, rgba(236,55,23,1) 100%)', 
+    position: 'relative',
+    backgroundColor: '#FBFAFA'
+    // height: '100vh'
+    // backgroundColor: 'linear-gradient(0deg, rgba(166,15,15,1) 0%, rgba(236,55,23,1) 100%)', 
   },
   viewText: {
-    height: 700,
+    // height: 700,
+    gap: 8,
+    paddingBottom: 20
 
   },
   textTitle: {
-    color: '#fff',
-    fontWeight: 600,
-   
+    color: '#E20821',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignContent: 'center',
+    alignItems: 'center'
+  },
+  informations: {
+    gap: 16
   },
   title: {
-    textAlign: 'start',
+    textAlign: 'center',
     fontWeight: 600,
-    color: '#fff',
-    paddingBottom: 20
+    color: '#E20821',
+    paddingBottom: 20,
   },
   textParagraph: {
-    color: '#FFF',
-    fontWeight: '700'
+    color: '#3F3F3F',
+    fontWeight: '700',
+    flex: 1,
+    fontSize: 14,
+    lineHeight: 24
+  },
+  textParagraphToRight: {
+    color: '#3F3F3F',
+    fontWeight: '700',
+    flex: 1,
+    textAlign: 'right',
+    fontSize: 14,
+    lineHeight: 24
+  },
+  simbolo :{
+    width: 80,
+    height: 80,
+    resizeMode: 'contain',
+    position: 'absolute',
+    right: 0,
+    top: -15,
+    zIndex: 10
+  },
+  pacoFrevo: {
+    // width: 100,
+    // flex: 1,
+    // height: 100,
+    resizeMode: 'contain',
+    alignSelf: 'flex-end', // Alinha à direita
+    marginLeft: 10, // Adiciona um espaçamento à esquerda para ajuste
+  },
+  contentAlignWithImage: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    alignItems: 'center',
+    alignContent: 'center'
+  },
+  detalhesImage: {
+    // width: '100%', // Adapta a largura à largura total do componente pai
+    // height: 200, // Adapte a altura conforme necessário
+    
+    // width: 200,
+    // height: 'auto',
+    // height: 80,
+    flex: 1,
+    resizeMode: 'contain',
+    resizeMode: 'cover', // ou 'contain', dependendo do comportamento desejado
+    marginBottom: 16, // Adicione um espaçamento inferior para separar do próximo parágrafo
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    borderTopWidth: 1,
+    borderTopColor: '#989898',
+    paddingBottom: 8,
+    paddingTop: 8,
   }
 });
 
--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,10 +1,13 @@
 // Import necessary components and modules
 import React, { useState } from 'react';
-import { View, StyleSheet, TouchableOpacity, Text, Image } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, Image, ImageBackground } from 'react-native';
 import { TextInput, Button, Snackbar } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 import LogoPF from '../../assets/logo-paco.png';
 import HomePaco from '../../assets/home-sobre-predio.png'
+import { Flyout } from '../components/Flyout';
+import { Righteous_400Regular } from '@expo-google-fonts/righteous';
+// import PacoFrevo from '../../assets/paco-do-frevo.JPG'
 
 // Create the LoginScreen component
 const LoginScreen = () => {
@@ -20,60 +23,103 @@ const LoginScreen = () => {
     });
     setSnackbarVisible(true);
   };
+  const [isFlyoutOpen, setFlyoutOpen] = useState(false);
+
+  const openFlyout = () => {
+    setFlyoutOpen(true);
+  };
+
+  const closeFlyout = () => {
+    setFlyoutOpen(false);
+  };
 
   return (
-    
-    <View style={styles.container}>
-      <Image source={HomePaco} style={{ width: 80, height: 90, display: 'flex', alignSelf: 'center', marginTop: 25 }} />
-      <View style={styles.inputsContainer}>
-        <TextInput
-          label="Digite seu E-mail"
-          value={username}
-          onChangeText={(text) => setUsername(text)}
-          contentStyle={{ borderRadius: 5, backgroundColor: '#FFF', borderWidth: 0 }}
-        />
-        <TextInput
-          label="Digite sua senha"
-          secureTextEntry
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          contentStyle={{ borderRadius: 5, backgroundColor: '#FFF', borderWidth: 0, }}
-        />
-      
-        <TouchableOpacity style={{ marginTop: 10, backgroundColor: '#DCDCDC', padding: 12, borderRadius: 5, }} onPress={handleLogin}>
-          <Text style={{ textAlign: 'center', color: '#000', fontSize: 15 }}>Acessar</Text>
-        </TouchableOpacity>
-        <Snackbar
-          visible={snackbarVisible}
-          onDismiss={() => setSnackbarVisible(false)}
-          action={{
-            label: 'OK',
-            onPress: () => setSnackbarVisible(false),
-          }}
-          style={{ marginBottom: -70, }}
-        >
-          Login realizado!
-        </Snackbar>
+    <ImageBackground source={HomePaco} style={styles.background}>
+      <View style={styles.container}>
+        {/* <Image source={HomePaco} style={{ width: 80, height: 90, display: 'flex', alignSelf: 'center', marginTop: 25 }} /> */}
+        <View style={styles.inputsContainer}>
+          <View style={styles.inputsWrapper}>
+            <Text style={styles.textStyle}>Email</Text>
+            <TextInput
+              label="Digite seu E-mail"
+              value={username}
+              onChangeText={(text) => setUsername(text)}
+              style={styles.input}
+              // contentStyle={{ borderRadius: 8, backgroundColor: '#FFF', borderWidth: 1, borderColor: '#989898' }}
+            />
+          </View>
+          <View style={styles.inputsWrapper}>
+            <Text style={styles.textStyle}>Senha</Text>
+            <TextInput
+              label="Digite sua senha"
+              secureTextEntry
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+              style={styles.input}
+              // contentStyle={{ borderRadius: 8, backgroundColor: '#FFF', borderWidth: 1, borderColor: '#989898' }}
+            />
+          </View>
+          <TouchableOpacity style={{ marginTop: 10, backgroundColor: '#27962D', padding: 12, borderRadius: 5, }} onPress={handleLogin}>
+            <Text style={{ textAlign: 'center', color: '#000', fontSize: 15 }}>Acessar</Text>
+          </TouchableOpacity>
+          <Snackbar
+            visible={snackbarVisible}
+            onDismiss={() => setSnackbarVisible(false)}
+            action={{
+              label: 'OK',
+              onPress: () => setSnackbarVisible(false),
+            }}
+            style={{ marginBottom: -70, }}
+          >
+            Login realizado!
+          </Snackbar>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
+    
   );
 };
 
 // Define the styles for the components
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover', // ou 'stretch' para cobrir toda a área
+    justifyContent: 'center', // ou 'flex-end', 'flex-start'
+    alignItems: 'center', // ou 'flex-end', 'flex-start'
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
-    padding: 16,
-    gap: 25,
-    
-    backgroundColor: 'linear-gradient(0deg, rgba(166,15,15,1) 0%, rgba(236,55,23,1) 100%)', 
+    padding: 25,
+    // gap: 25,
+    height: '100%',
+    width: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)'
+    // backgroundColor: 'linear-gradient(0deg, rgba(166,15,15,1) 0%, rgba(236,55,23,1) 100%)', 
   },
   inputsContainer: {
     justifyContent: 'center',
-    padding: 10,
-    paddingBottom: 32,
+    padding: 30,
+    // paddingBottom: 32,
     gap: 25,
+    backgroundColor: '#D3D3D3',
+    borderRadius: 8,
+  },
+  inputsWrapper: {
+    gap: 8,
+  },
+  textStyle: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#3f3f3f'
+  },
+  input: {
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#989898',
+    underlineColor: 'transparent', // Isso remove o underline padrão
   },
 });
 
